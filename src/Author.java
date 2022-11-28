4bf53f6d435fe1f9e882b689335edb01c161633f
Wilson Syring <wilson.syring@gmail.com>
2022-11-28 14:27:54 -0600

    forked and created a repo for annotations

--- src/Author.java
+++ src/Author.java
@@ -1,12 +1,14 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
+@SuppressWarnings
 import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
+    @SuppressWarnings
     private List books;
-
+    @SuppressWarnings
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
         books = new ArrayList();
@@ -15,15 +17,15 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
-    @Deprecated
+    @deprecated
     public List<String> getBooks() {
         return books;
     }
-
+    @SuppressWarnings
     public List<String> publishedBooks() {
         return books;
     }
-
+    @SuppressWarnings
     public void addBook(String book) {
         books.add(book);
     }
